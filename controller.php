aa7232171c74e0e931d05d03cc6f86924856fb8f
Markus Liechti <kaapiii@hotmail.com>
2019-02-12 20:10:28 +0100

    fix error of a missing MySQL function configuration, which appeared when the plugin was installed with a composer file in the project root directory

--- controller.php
+++ controller.php
@@ -2,6 +2,7 @@
 
 namespace Concrete\Package\Concrete5DoctrineDqlExtensions;
 
+use Concrete\Core\Support\Facade\Log;
 use Doctrine\Common\EventManager;
 use Symfony\Component\Yaml\Yaml;
 use Symfony\Component\Yaml\Exception\ParseException;
@@ -14,10 +15,9 @@ use Symfony\Component\Yaml\Exception\ParseException;
  */
 class Controller extends \Concrete\Core\Package\Package
 {
-
-    protected $pkgHandle = 'concrete5_doctrine_dql_extensions';
+    protected $pkgHandle          = 'concrete5_doctrine_dql_extensions';
     protected $appVersionRequired = '8.0.0';
-    protected $pkgVersion = '0.2.1';
+    protected $pkgVersion         = '0.2.0';
 
     public function getPackageDescription()
     {
@@ -32,26 +32,35 @@ class Controller extends \Concrete\Core\Package\Package
     public function install()
     {
         $pkg = parent::install();
-        \Concrete\Core\Page\Single::add('/dashboard/system/doctrine_dql_extensions', $pkg);
+        \Concrete\Core\Page\Single::add('/dashboard/system/doctrine_dql_extensions',$pkg);
     }
 
     public function on_start()
     {
         // register the autoloading
-        if (file_exists($this->getPackagePath() . '/vendor/autoload.php')) {
+        if(file_exists($this->getPackagePath() . '/vendor/autoload.php')){
             require $this->getPackagePath() . '/vendor/autoload.php';
         }
 
+        /** @var \Doctrine\ORM\EntityManager $em */
         $em = $this->app->make('Doctrine\ORM\EntityManager');
+        /** @var \Doctrine\ORM\Configuration $config */
         $config = $em->getConfiguration();
-        $this->registerDoctrineDqlExtensions($config);
+        try{
+            $this->registerDoctrineDqlExtensions($config);
+        }catch(\Doctrine\ORM\ORMException $e){
+            Log::addAlert('While adding Doctrine DQL extensions to the EntityManager configuration something went wrong: '. $e);
+        }
+
     }
+
 
     /**
      * Register Doctrine2 dql extensions
      *
-     * @param EventManager $evm
-     * @param Reader $cachedAnnotationReader
+     * @param $config \Doctrine\ORM\Configuration
+     * @return \Doctrine\ORM\Configuration mixed
+     * @throws \Doctrine\ORM\ORMException
      */
     public function registerDoctrineDqlExtensions($config)
     {
@@ -63,18 +72,18 @@ class Controller extends \Concrete\Core\Package\Package
         $datetimeFunctions = $dqlFunctions['datetime_functions'];
         $numericFunctions = $dqlFunctions['numeric_functions'];
         $stringFunctions = $dqlFunctions['string_functions'];
-        if (count($datetimeFunctions)) {
-            foreach ($datetimeFunctions as $name => $class) {
+        if(count($datetimeFunctions)){
+            foreach($datetimeFunctions as $name => $class){
                 $config->addCustomDatetimeFunction($name, $class);
             }
         }
-        if (count($numericFunctions)) {
-            foreach ($numericFunctions as $name => $class) {
+        if(count($numericFunctions)){
+            foreach($numericFunctions as $name => $class){
                 $config->addCustomNumericFunction($name, $class);
             }
         }
-        if (count($stringFunctions)) {
-            foreach ($stringFunctions as $name => $class) {
+        if(count($stringFunctions)){
+            foreach($stringFunctions as $name => $class){
                 $config->addCustomStringFunction($name, $class);
             }
         }
@@ -83,49 +92,43 @@ class Controller extends \Concrete\Core\Package\Package
 
     /**
      * Parse yaml config of MySQL doctrine dql extensions
-     * 
+     *
      * @return array
      */
-    protected function parseDoctrineQueryExtensionConfig()
-    {
+    protected function parseDoctrineQueryExtensionConfig(){
         try {
             $config = Yaml::parse(file_get_contents($this->getMysqlConfig()));
         } catch (ParseException $e) {
-            throw new ParseException('The yaml configuration for berberlei/DoctrineExtensions couldn\'t be parsed. '. $e);
+            //printf("Unable to parse the YAML string: %s", $e->getMessage());
         }
         return $config;
     }
 
     /**
-     * Get path to MySQL yaml config
-     * 
-     * Search eather in {package-root}/vendor or in {root}/concrete5/vendor
+     * Get path to MySQL yaml config the vendor
+     * It takes into account how the package was installed
      *
-     * @throws \Exception
      * @return string
      */
-    protected function getMysqlConfig()
-    {
+    protected function getMysqlConfig(){
+        // Path to the mysql config, if package was installed manually, and the
+        // package contains a 'vendor' directory
+        $localVendorPath = $this->getPackagePath(). DIRECTORY_SEPARATOR . DIRNAME_VENDOR
+            . DIRECTORY_SEPARATOR . 'beberlei' . DIRECTORY_SEPARATOR
+            . 'DoctrineExtensions' . DIRECTORY_SEPARATOR . 'config'
+            . DIRECTORY_SEPARATOR . 'mysql.yml';
 
-        $pathPackage = $this->getPackagePath() . DIRECTORY_SEPARATOR . 'vendor'
-                . DIRECTORY_SEPARATOR . 'beberlei' . DIRECTORY_SEPARATOR
-                . 'DoctrineExtensions' . DIRECTORY_SEPARATOR . 'config'
-                . DIRECTORY_SEPARATOR . 'mysql.yml';
+        $globalVendorPath = DIR_BASE_CORE . DIRECTORY_SEPARATOR . DIRNAME_VENDOR
+            . DIRECTORY_SEPARATOR . 'beberlei' . DIRECTORY_SEPARATOR
+            . 'DoctrineExtensions' . DIRECTORY_SEPARATOR . 'config'
+            . DIRECTORY_SEPARATOR . 'mysql.yml';
 
-        $pathVendor = DIR_BASE . DIRECTORY_SEPARATOR . DIRNAME_CORE
-                . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR
-                . 'beberlei' . DIRECTORY_SEPARATOR
-                . 'DoctrineExtensions' . DIRECTORY_SEPARATOR . 'config'
-                . DIRECTORY_SEPARATOR . 'mysql.yml';
-
-        if (file_exists($pathPackage)) {
-            $path = $pathPackage;
-        } elseif (file_exists($pathVendor)) {
-            $path = $pathVendor;
-        } else {
-            throw new \Exception('Yaml config file for DonctrineExtensions was not found. Searched unter: '. $pathPackage .' - and -' . $pathVendor);
+        if(file_exists($localVendorPath)){
+            $path = $localVendorPath;
+        }else{
+            $path = $globalVendorPath;
         }
+
         return $path;
     }
-
 }
